bae3150d8e95ac9d318c7c14f2fd766b9da4be0f
mark-williams <markwill@hotmail.co.uk>
2018-02-04 09:51:21 +0000

    Creates store with imported reducer from redux-form

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
 import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
-const store = createStore(
-  (state = {}) => (state),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const reducers = combineReducers(
+  {
+    dummy: (state = {}) => (state),
+    form: formReducer
+  }
 );
 
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
