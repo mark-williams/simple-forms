90df59498ea139706b1d9ce64f4733c566e9de73
mark-williams <markwill@hotmail.co.uk>
2018-02-04 09:42:23 +0000

    fix: casing for <Fragment>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { SectionHead } from '../style';
 
 export default () => (
-  <fragment>
+  <Fragment>
     <SectionHead>Quam Vehicula Elementum</SectionHead>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
     <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
     <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
-  </fragment>
+  </Fragment>
 );
