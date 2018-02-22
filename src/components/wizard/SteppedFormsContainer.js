213683a31027702cf842790a9438d67342eb1c51
mark-williams <markwill@hotmail.co.uk>
2018-02-22 09:13:41 +0000

    wizard now more generic - accepts steps as an array

--- src/components/wizard/SteppedFormsContainer.js
+++ src/components/wizard/SteppedFormsContainer.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Wizard from './Wizard';
 
+const steps = [
+  { name: 'Name' },
+  { name: 'Address' },
+  { name: 'Additional' }
+];
+
 class SteppedFormsContainer extends React.Component {
   render = () => {
     const basePath = this.props.match.path;
     return (
-      <Wizard basePath={basePath}/>
+      <Wizard basePath={basePath} steps={steps} />
     );
   }
 }
--- src/components/wizard/Wizard.js
+++ src/components/wizard/Wizard.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import { Route, Switch, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -11,10 +10,9 @@ const WizardNavigation = styled.div`
 const WizardNavigationItem = styled.div`
   margin-right: 1rem;
   margin-bottom: 1.6rem;
-  a {
+  div {
     font-size: 1.2rem;
     color: hsl(0, 0%, 40%);
-    text-decoration: none;
     &.active {
       border-bottom: 3px solid hsl(0, 0%, 60%)
     }
@@ -30,7 +28,6 @@ const WizardButtons = styled.div`
   justify-content: flex-end;
 `;
 
-const getPath = (basePath, stepName) => ( `${basePath}/${stepName}` );
 class Wizard extends React.Component {
   state = { currentStep: 0 };
 
@@ -41,35 +38,31 @@ class Wizard extends React.Component {
   moveNext = this.makeStepMover(1);
 
   render = () => {
-    const { basePath } = this.props;
-
     return (
       <Fragment>
         <h2>Forms Wizard</h2>
         <div>
           <WizardNavigation>
-            <WizardNavigationItem><NavLink to={getPath(basePath, 'name')} activeClassName="active">Name</NavLink></WizardNavigationItem>
-            <WizardNavigationItem><NavLink to={getPath(basePath, 'address')} activeClassName="active">Address</NavLink></WizardNavigationItem>
+            { this.props.steps.map((s, i) => <WizardNavigationItem><div key={s.name} className={i === this.state.currentStep ? 'active' : null}>{s.name}</div></WizardNavigationItem>)}
           </WizardNavigation>
         </div>
         <StepContainer>
-          <Switch>
-            <Route exact path={getPath(basePath, 'name')} render={() => <div>Capture name</div>} />
-            <Route exact path={getPath(basePath, 'address')} render={() => <div>Capture address</div>} />
-          </Switch>
+          <div>
+            Step { this.state.currentStep}
+          </div>
           <WizardButtons>
             <button onClick={this.movePrev}>Back</button>
             <button onClick={this.moveNext}>Next</button>
           </WizardButtons>
         </StepContainer>
-        Current step is { this.state.currentStep }
       </Fragment>
     );
   }
 }
 
 Wizard.propTypes = {
-  basePath: PropTypes.string
+  basePath: PropTypes.string,
+  steps: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default Wizard;
